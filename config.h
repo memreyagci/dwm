fb63800302ff074b0eb545b0c0a098b9fac765ea
memreyagci <meydeveloper@tutanota.com>
2020-12-11 20:17:47 +0100

    Font Awesome replaced with Noto Color Emoji

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 20;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=13:antialias=true:autohint=true", "Font Awesome 5 Free Solid:size=10:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=13:antialias=true:autohint=true", "Noto Color Emoji:size=10" };
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
