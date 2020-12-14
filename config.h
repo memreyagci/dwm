5fa4946ab1c02060964e54fe476c641db55b1512
memreyagci <meydeveloper@tutanota.com>
2020-12-14 23:08:41 +0100

    urgentborder patch is added since it is required by pywal

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+static const char urgbordercolor[]  = "#ff0000";
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
