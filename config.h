99a4a8d9fd499a1bb2f87d7ccef27c3f346e5fef
memreyagci <meydeveloper@tutanota.com>
2020-12-26 20:48:59 +0100

    MODKEY+ShiftMask+q shortcut is set to power-management-options script

--- config.h
+++ config.h
@@ -147,7 +147,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      spawn,          SHCMD("power-management-options")},
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
     
 	{ 0, XF86XK_AudioMute,		    spawn,		SHCMD("pactl set-sink-mute 0 toggle; kill -44 $(pidof dwmblocks)") },
